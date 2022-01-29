1593c86aa42d63d28f2d795be0997255be5a38c8
Nathan Luong <nhanto20021@gmail.com>
2022-01-28 21:04:02 -0500

    lol

--- frontend/components/ChessBoard.jsx
+++ frontend/components/ChessBoard.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import * as ChessJS from "chess.js";
 
-export default function ChessBoard(){
+ function ChessBoard(){
     //Chess board is 8x8
     
+    const Chess = typeof ChessJS === "function" ? ChessJS : ChessJS.Chess;
+    const chess = new Chess();
+    const [chessFen, setChessFen] = useState(chess.ascii());
+    console.log(chessFen);
+    chess.move("e4");
+    console.log(chess.ascii());
+    console.log(chess.board());
 
-
+    return(
+        <div>
+            <h1>Chess Board</h1>
+        </div>
+    )
 }
 
-
+export default ChessBoard;
